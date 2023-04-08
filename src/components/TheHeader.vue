67a3969cf068038964c0889bd62f79490222188f
Larissa <larissa@latouraineinc.com>
2023-04-07 18:22:12 -0600

    Add navigation menu in header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,8 +2,13 @@
   <header class="header--wrapper">
     <div class="header">
       <div class="header--logo"></div>
-      <div class="header--navigation"></div>
+      <div class="header--navigation">
+        <the-nav></the-nav>
+      </div>
       <div class="header--auth"></div>
     </div>
   </header>
 </template>
+<script setup>
+import TheNav from './TheNav.vue';
+</script>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="thenav">
+    <li class="thenav--item">
+      <a class="thenav--link selected" href="">Generate</a>
+      <a class="thenav--link" href="">My Images</a>
+      <a class="thenav--link" href="">Favorites</a>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "TheNav",
+  inheritAttrs: false,
+};
+</script>
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: "",
+  },
+  link: {
+    type: String,
+    default: "",
+  },
+});
+</script>
